6aae3fa658b32659242b35aa57bf72265ba0b3ee
msaaddev <mrsaadirfan@gmail.com>
2021-07-01 03:28:31 +0500

    👌 IMPROVE: tests

--- test.js
+++ test.js
@@ -1,19 +1,21 @@
 const end = require('./index');
 const handleError = require('node-cli-handle-error');
+const clearConsole = require('clear-any-console');
 const pkgJSON = require('./package.json');
 
 (async () => {
 	try {
+		clearConsole();
 		// es5 syntax
 		await end({
-			gitHub: `https://github.com/msaaddev`,
+			github: `https://github.com/msaaddev`,
 			twitter: `http://twitter.com/msaaddev`,
 			pkgJSON: pkgJSON
 		});
 
 		// es6 syntax
 		await end({
-			gitHub: `https://github.com/msaaddev`,
+			github: `https://github.com/msaaddev`,
 			twitter: `http://twitter.com/msaaddev`,
 			pkgJSON
 		});
